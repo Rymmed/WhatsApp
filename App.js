ab24e9cc1691227f7d993dd6a7e5fc39c7e17ccf
RymMohamed <mhamedrim921@gmail.com>
2023-12-09 22:25:00 +0100

    chat screen

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/ChatListItem';
+import ChatsScreen from './src/screens/ChatsScreen';
 import ChatScreen from './src/screens/ChatScreen';
-const chat = {
-  id: "1",
-  user: {
-    image:
-    "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-    name: "Lukas",
-  },
-  lastMessage: {
-    text: "Oke",
-    createdAt: "07:30",
-  }
-}
-
 
 export default function App() {
   return (
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,14 +1,26 @@
-import { View, Text, FlatList } from "react-native";
-import chats from "../../assets/data/chats.json";
-import ChatListItem from "../components/ChatListItem";
+import { View, Text, ImageBackground,StyleSheet, FlatList } from 'react-native'
+import bg from '../../assets/images/BG.png';
+import Message from '../components/Message';
+import messages from '../../assets/data/messages.json';
 
 const ChatScreen = () => {
   return (
-    <FlatList
-      data={chats}
-      renderItem={({ item }) => <ChatListItem chat={item} />}
-    />
-  );
-};
-
-export default ChatScreen;
+    <ImageBackground source={bg} style={styles.bg}>
+      <FlatList
+        data ={messages}
+        renderItem={({ item }) => <Message message={item} />}
+        style={styles.list}
+        inverted
+      />
+    </ImageBackground>
+  )
+}
+const styles = StyleSheet.create({
+    bg: {
+       flex: 1, 
+    },
+    list:{
+        padding: 10,
+    }
+})
+export default ChatScreen
